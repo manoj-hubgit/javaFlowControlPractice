254e4b498d739096c5e31232015f2bf18532fbed
manoj-hubgit <manojannadurai2265@gmail.com>
2024-12-24 10:30:25 +0530

    added

--- Main.java
+++ Main.java
@@ -149,8 +149,8 @@ class Quad{
         System.out.print("Enter the third number : ");
         double c=userInput.nextInt();
         double discriminant= b*2-4*a*c;
-        if(discriminant>0){                       //-b + math.sqrt(discriminant)/2*a
-           double root1= (-b + Math.sqrt(discriminant))/(2*a);     //-b - math.sqrt(discriminant)/2*a
+        if(discriminant>0){                       
+           double root1= (-b + Math.sqrt(discriminant))/(2*a);    
            double root2= (-b + Math.sqrt(discriminant))/(2*a);
            System.out.println(root1+" "+ root2);
         }else if(discriminant==0){
